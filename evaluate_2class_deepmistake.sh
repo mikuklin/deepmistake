aa3d205a9116c4a16e3e1d281872595bd01665ed
Mikhail kuklin <kuklinmike02@gmail.com>
2024-10-18 19:53:13 +0300

    removed trailing slash

--- evaluate_2class_deepmistake.sh
+++ evaluate_2class_deepmistake.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-ckpt_path=$1
-input_dir=$2
-output_dir=$3
 pip install krippendorff==0.8.0
+ckpt_path=${1%/}
+input_dir=${2%/}
+output_dir=${3%/}
+
 python inference_deepmistake.py  --deepmistake_dir $ckpt_path --input_dir $input_dir
 mkdir -p scores/${input_dir}/2class/${output_dir}/
 python evaluation.py $input_dir scores/${input_dir}/2class/${output_dir}/
--- evaluate_2class_treshold_deepmistake.sh
+++ evaluate_2class_treshold_deepmistake.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-ckpt_path=$1
-dir=$2
-theshold=$3
-output_dir=$4
+ckpt_path=${1%/}
+dir=${2%/}
+theshold=${3%/}
+output_dir=${4%/}
+
 python inference_deepmistake.py  --deepmistake_dir $ckpt_path --input_dir $dir --treshold_dir $theshold --mode 2class_treshold
 mkdir -p scores/${dir}/2class_treshold/${output_dir}/
 python evaluation.py $dir scores/${dir}/2class_treshold/${output_dir}/
