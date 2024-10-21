fixed KLDivLoss: it should take predicted probabilities in the log space, not logits; also targets contain label frequencies not in the log spce, so should just normalize them instead of doing softmax

deepmistake/xlmr.py
@@ -189,7 +189,9 @@ class XLMRModel(BertPreTrainedModel):
                 loss['total'] = CrossEntropyLoss()(syn_logits, syn_labels.type(torch.int64))
             elif self.local_config['loss'] == 'kl_divergence_loss':
                 print("KL_input: ", syn_logits, syn_labels)
-                loss['total'] = KLDivLoss()(syn_logits, torch.tensor(F.softmax(syn_labels.float(), dim=-1)))
+#                import pdb; pdb.set_trace()
+                loss['total'] = KLDivLoss()(F.log_softmax(syn_logits, dim=-1), syn_labels / syn_labels.sum(dim=-1, keepdim=True))
+                print(loss['total'])
             else:
                 loss['total'] = CosineEmbeddingLoss()(syn_logits[0], syn_logits[1], syn_labels * 2 - 1)
 